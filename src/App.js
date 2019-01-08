7543f46f9f0fde9110487fa35938bd5f46dbb150
romfol <romanfoltyn1@gmail.com>
2019-01-08 20:11:23 +0200

    basic map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
-import Map from './components/Map/Map';
+import MapContainer from './components/Map/Map';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Map />
+      <MapContainer />
     </div>
   );
 };
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class Map extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
 
+export class MapContainer extends Component {
   render() {
-    return <h1>here im map11</h1>;
+    if (!this.props.loaded) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <Map
+        google={this.props.google}
+        zoom={4}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233,
+        }}
+      />
+    );
   }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCVqpw6Ba0O5h3wLdeukvc7Lvuf_zIVYW4',
+})(MapContainer);
