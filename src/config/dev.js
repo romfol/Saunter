0b3fc5c03e402fb9c162eec54ef2e8709e3903a4
romfol <romanfoltyn1@gmail.com>
2019-01-27 18:51:02 +0200

    fix

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyAiK4HzB4f73g2babMwXD3trd8NcCB3CKQ',
-  authDomain: 'my-project-1513188578457.firebaseapp.com',
-  databaseURL: 'https://my-project-1513188578457.firebaseio.com',
-};
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,10 @@
 import * as firebase from 'firebase';
 
-import { FirebaseConfig } from './keys';
+const FirebaseConfig = {
+  apiKey: 'AIzaSyAiK4HzB4f73g2babMwXD3trd8NcCB3CKQ',
+  authDomain: 'my-project-1513188578457.firebaseapp.com',
+  databaseURL: 'https://my-project-1513188578457.firebaseio.com',
+};
 
 firebase.initializeApp(FirebaseConfig);
 
